615607678d3d421de1f1f6840811ef01195743aa
Pranav <pranav.fokmare.developer@gmail.com>
2022-07-12 21:32:37 +0530

    AlgoBreaker will be on by start

--- popup.js
+++ popup.js
@@ -5,11 +5,11 @@ button.addEventListener("click", async () => {
 
   chrome.storage.sync.get(["mode"], function (result) {
     const mode = result.mode;
-    const changedMode = mode === "on" ? "off" : "on";
+    const toggledMode = mode === "on" ? "off" : "on";
     button.innerHTML = `Turn it ${mode}`;
-    chrome.storage.sync.set({ mode: changedMode }, function () {
+    chrome.storage.sync.set({ mode: toggledMode }, function () {
       chrome.runtime.sendMessage(
-        { tabId: tab.id, tabUrl: tab.url, mode: changedMode },
+        { tabId: tab.id, tabUrl: tab.url, mode: toggledMode },
         function (response) {}
       );
     });
