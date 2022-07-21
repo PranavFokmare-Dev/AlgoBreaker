82528d1ef40b1ed201499c8ec5869ecac6c3e038
Pranav <pranav.fokmare.developer@gmail.com>
2022-07-21 18:51:26 +0530

    formatted and refactored code

--- background.js
+++ background.js
@@ -1,18 +1,17 @@
 const analyticsEnum = {
-  NoTabSet : -1,
-  newTabUrl :"chrome://newtab/",
-  emptyUrl :"EMPTY_URL",
-  historyRemoverAlarmName : "historyWeeklyRemover"
-}
+  NoTabSet: -1,
+  newTabUrl: "chrome://newtab/",
+  emptyUrl: "EMPTY_URL",
+  historyRemoverAlarmName: "historyWeeklyRemover",
+};
 const webPage = {
-  homePage:
-    "#primary > ytd-rich-grid-renderer",
+  homePage: "#primary > ytd-rich-grid-renderer",
   videoPlayerEndScreen:
     "#movie_player > div.html5-endscreen.ytp-player-content.videowall-endscreen.ytp-show-tiles",
   videoPlayerSideContent: "#items > ytd-item-section-renderer",
   search: "#page-manager > ytd-search",
-  playlistSideContent :"#items > ytd-item-section-renderer",
-  videoPlayerSideContent2:"#secondary",
+  playlistSideContent: "#items > ytd-item-section-renderer",
+  videoPlayerSideContent2: "#secondary",
 };
 
 let currentTabId = -1;
@@ -21,14 +20,20 @@ const tabSessions = {};
 chrome.runtime.onInstalled.addListener(async () => {
   chrome.storage.sync.set({ mode: "on" }, function () {});
   console.log("runtime on installed");
-  await setInStorage({mode:"on"});
+  await setInStorage({ mode: "on" });
   await saveHistory({});
-  const weekDurationInMins = 7 * 24* 60;
-  chrome.alarms.create(analyticsEnum.historyRemoverAlarmName, {periodInMinutes:weekDurationInMins});
+  const weekDurationInMins = 7 * 24 * 60;
+  chrome.alarms.create(analyticsEnum.historyRemoverAlarmName, {
+    periodInMinutes: weekDurationInMins,
+  });
 });
 
 //Button click -> on/off call
-chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(async function (
+  request,
+  sender,
+  sendResponse
+) {
   AlgoBreakerMain(request.mode, request.tabId, request.tabUrl);
 
   console.log("SUMMARY");
@@ -44,9 +49,6 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     AlgoBreakerMain(mode, tab.id, tab.url);
   });
 });
-
-
-
 
 function AlgoBreakerMain(mode, tabId, url) {
   if (mode === "on") AlgoBreakerOn(url, tabId);
@@ -89,142 +91,154 @@ function AlgoBreakerOff(tabId) {
   );
 }
 
-
-
 //Analytics code
 
+chrome.tabs.onActivated.addListener(async function (activeInfo) {
+  console.log("Active" + activeInfo.tabId);
+  let tabId = activeInfo.tabId;
+  await endOldSession();
 
+  currentTabId = tabId;
+  const isOldTab = tabSessions[tabId] !== undefined;
+  if (isOldTab) {
+    tabSessions[tabId].startTime = Date.now();
+  } else {
+    tabSessions[tabId] = {
+      url: analyticsEnum.emptyUrl,
+      startTime: 0,
+      endTime: 0,
+    };
+  }
 
-chrome.tabs.onActivated.addListener(
-    async function(activeInfo){
-      console.log("Active" + activeInfo.tabId);
-        let tabId = activeInfo.tabId;
-        if(currentTabId !== analyticsEnum.NoTabSet){
-            await endTabSession(currentTabId);
-        }
-      currentTabId = tabId;
-      const isOldTab = tabSessions[tabId]!== undefined
-      if(isOldTab){
-        tabSessions[tabId].startTime = Date.now();
-      }
-      else{
-        tabSessions[tabId] = {
-          url:analyticsEnum.emptyUrl,
-          startTime: 0,
-          endTime :0
-       };
-      
-      }
+  async function endOldSession() {
+    if (currentTabId !== analyticsEnum.NoTabSet) {
+      await endTabSession(currentTabId);
     }
-   )
- chrome.tabs.onUpdated.addListener( async function (tabId, changeInfo, tab) {
-  const notUpdatedToNewTab = tab.url!=analyticsEnum.newTabUrl 
-  
-  console.log("update" + tabId);
-  if(notUpdatedToNewTab){
+  }
+});
+chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
+  const notUpdatedToNewTab = tab.url != analyticsEnum.newTabUrl;
 
-     if(tabSessions[tabId]!== undefined ){
-      const linkUpdatedOnNewTab = tabSessions[tabId].url === analyticsEnum.emptyUrl
-       if(linkUpdatedOnNewTab){
-         tabSessions[tabId].url = getHostName(tab.url);
-         tabSessions[tabId].startTime = Date.now();
-       }
-       else{
-          //In current tab user changed the link
-         if(tabId === currentTabId){
-           await endTabSession(tabId); 
-           tabSessions[tabId] = {url:getHostName(tab.url),startTime:Date.now(), endTime:0};
-         }
-       }
-     }
-     else{
+  console.log("update" + tabId);
+  if (notUpdatedToNewTab) {
+    await handleTabLoadedEvent();
+  }
+
+  async function handleTabLoadedEvent() {
+    const tabExists = tabSessions[tabId] !== undefined;
+    if (tabExists) {
+      const linkUpdatedOnNewTab =
+        tabSessions[tabId].url === analyticsEnum.emptyUrl;
+      if (linkUpdatedOnNewTab) {
+        initializeSession();
+      } else {
+        //In current tab user changed the link
+        await updateSessionToNewLink();
+      }
+    } else {
       //middle click
-      console.log("middle click");
-      console.log(tab);
-      tabSessions[tabId] = {url:getHostName(tab.url),startTime:0, endTime:0};
-     }
-   }
- })
+      initalizeInactiveSession();
+    }
+  }
+
+  function initializeSession() {
+    tabSessions[tabId].url = getHostName(tab.url);
+    tabSessions[tabId].startTime = Date.now();
+  }
+
+  async function updateSessionToNewLink() {
+    if (tabId === currentTabId) {
+      await endTabSession(tabId);
+      tabSessions[tabId] = {
+        url: getHostName(tab.url),
+        startTime: Date.now(),
+        endTime: 0,
+      };
+    }
+  }
+
+  function initalizeInactiveSession() {
+    console.log("middle click");
+    console.log(tab);
+    tabSessions[tabId] = {
+      url: getHostName(tab.url),
+      startTime: 0,
+      endTime: 0,
+    };
+  }
+});
 async function endTabSession(tabId) {
   const session = tabSessions[tabId];
   const timeSpent = Date.now() - session.startTime;
-  let history = await getHistory();
-  if (history[session.url] === undefined) {
-    history[session.url] = 0;
-  }
-  history[session.url] += Math.max(timeSpent, 0);
+  await updateHistory();
   session.startTime = 0;
-  await saveHistory(history);
+
+  async function updateHistory() {
+    let history = await getHistory();
+    if (history[session.url] === undefined) {
+      history[session.url] = 0;
+    }
+    history[session.url] += Math.max(timeSpent, 0);
+    await saveHistory(history);
+  }
 }
 
-chrome.tabs.onRemoved.addListener(
- async function(tabId, removedInfo){
-  
+chrome.tabs.onRemoved.addListener(async function (tabId, removedInfo) {
   console.log("closed" + tabId);
-     if(tabSessions[tabId] !== undefined){
-        if(tabId == currentTabId) await endTabSession(tabId);
-        delete tabSessions[tabId]
-     }
-   }
- )
+  const tabExists = tabSessions[tabId] !== undefined;
+  if (tabExists) {
+    if (tabId == currentTabId) await endTabSession(tabId);
+    delete tabSessions[tabId];
+  }
+});
 
-async function getHistory(){
+async function getHistory() {
   let history = await getFromStorage("history");
-  return (history === undefined)?{}:history;
- }
-
-async function saveHistory(history){
-    await setInStorage({history:history});
+  return history === undefined ? {} : history;
 }
 
-function getFromStorage(key){
+async function saveHistory(history) {
+  await setInStorage({ history: history });
+}
+
+function getFromStorage(key) {
   return new Promise((resolve, reject) => {
-    chrome.storage.sync.get([key], function(result){
+    chrome.storage.sync.get([key], function (result) {
       const value = result[key];
-      if(chrome.runtime.lastError){
-        console.log("error occured"+chrome.runtime.error);
-      }
-      else{
+      if (chrome.runtime.lastError) {
+        console.log("error occured" + chrome.runtime.error);
+      } else {
         resolve(value);
       }
-    })
-
-  }); 
-}
-
-
-function setInStorage(data){
-  return new Promise((resolve, reject) => {
-    chrome.storage.sync.set(data, function() {
-      resolve();
     });
-    
-
-  }); 
-}
-
-function getTabInfo(tabId){
-  return new Promise((resolve, reject)=>{
-    chrome.tabs.get(tabId,function(tab){
-      resolve(tab);
-    })
   });
 }
 
-function getHostName(url){
+function setInStorage(data) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.set(data, function () {
+      resolve();
+    });
+  });
+}
+
+function getTabInfo(tabId) {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.get(tabId, function (tab) {
+      resolve(tab);
+    });
+  });
+}
+
+function getHostName(url) {
   const details = new URL(url);
   return details.hostname;
 }
 
-
 // history remover
-chrome.alarms.onAlarm.addListener(
-  async function(alarm){
-    if(alarm.name === analyticsEnum.historyRemoverAlarmName){
-      console.log("removing History");
-      await saveHistory({});
-    }
-   
+chrome.alarms.onAlarm.addListener(async function (alarm) {
+  if (alarm.name === analyticsEnum.historyRemoverAlarmName) {
+    console.log("removing History");
+    await saveHistory({});
   }
-)
-
+});
